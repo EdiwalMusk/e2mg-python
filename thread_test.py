0b617f38aac22d7bcc49ed3adeea55016ea5353b
zhangchenchengit <1520406629@qq.com>
2024-09-28 17:06:57 +0800

    11

--- thread_test.py
+++ thread_test.py
@@ -1,4 +1,21 @@
 import threading
+import time
 
-thread_obj = threading.Thread()
-thread_obj.start()
+
+def sing(msg):
+    while True:
+        print("sing!", msg)
+        time.sleep(1)
+
+
+def dance(msg):
+    while True:
+        print("dance!", msg)
+        time.sleep(1)
+
+
+if __name__ == '__main__':
+    thread_obj = threading.Thread(target=sing, args="a")
+    thread_obj.start()
+    thread_obj = threading.Thread(target=dance, kwargs={"msg": "b"})
+    thread_obj.start()
